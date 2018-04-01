8bc669bca4638f5ecc9e513c1498a7bedd0d6459
regbake <regbake@gmail.com>
2018-04-01 10:55:14 -0700

    router working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('450ms', [
+          animate('.5s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('450ms', [
+          animate('.5s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = "add an item";
   goalText: string = "first goal";
-  goals = [];
+  goals = ["send early", "send often"];
 
   constructor() { }
 
@@ -22,6 +44,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = ' ';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
