0440ccb0fb2a2536a6f9fc2e06e8a30b15be5f25
regbake <regbake@gmail.com>
2018-04-01 09:49:00 -0700

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number = 4;
+  btnText: string = "add an item";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
